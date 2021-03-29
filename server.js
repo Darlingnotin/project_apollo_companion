8b934578a8c6e002b3bb0a221b5929afdd1dabd1
Darlingnotin <darlingnotin@hotmail.com>
2021-03-29 10:13:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -47,73 +47,102 @@ function runServer() {
         fs.mkdirSync(serverFolder);
     }
     http.createServer(function (request, response) {
-        var uri = url.parse(request.url).pathname;
-        if (uri == "/" || uri == "/domains.json") {
-            pageData = [];
-            var rawdata = fs.readFileSync('Entities/Domains/Domains.json');
-            DomainsJson = JSON.parse(rawdata);
-            fs.readdir(serverFolder, (err, files) => {
-                if (files.length === 0) {
-                    sendPage(JSON.stringify(pageData));
-                } else {
-                    files.forEach(file => {
-                        var rawServerInformationdata = fs.readFileSync("Entities/Domain/" + file);
-                        serverInformation = JSON.parse(rawServerInformationdata);
-                        var currentDay = new Date();
-                        var oldDay = new Date(serverInformation.TimeOfLastHeartbeat);
-                        var DomainName = serverInformation.PlaceName;
-                        if (currentDay.getTime() - oldDay.getTime() <= 30000) {
-                            var domainExists = false;
-                            for (let i = 0; i < DomainsJson.length; i++) {
-                                if (serverInformation.DomainID == DomainsJson[i].DomainID) {
-                                    domainExists = true;
-                                    if (DomainsJson[i].DomainName != "") {
-                                        DomainName = DomainsJson[i].DomainName;
+        if (request.method == "GET") {
+            var uri = url.parse(request.url).pathname;
+            if (uri == "/" || uri == "/domains.json") {
+                pageData = [];
+                var rawdata = fs.readFileSync('Entities/Domains/Domains.json');
+                DomainsJson = JSON.parse(rawdata);
+                fs.readdir(serverFolder, (err, files) => {
+                    if (files.length === 0) {
+                        sendPage(JSON.stringify(pageData));
+                    } else {
+                        files.forEach(file => {
+                            var rawServerInformationdata = fs.readFileSync("Entities/Domain/" + file);
+                            serverInformation = JSON.parse(rawServerInformationdata);
+                            var currentDay = new Date();
+                            var oldDay = new Date(serverInformation.TimeOfLastHeartbeat);
+                            var DomainName = serverInformation.PlaceName;
+                            if (currentDay.getTime() - oldDay.getTime() <= 30000) {
+                                var domainExists = false;
+                                for (let i = 0; i < DomainsJson.length; i++) {
+                                    if (serverInformation.DomainID == DomainsJson[i].DomainID) {
+                                        domainExists = true;
+                                        if (DomainsJson[i].DomainName != "") {
+                                            DomainName = DomainsJson[i].DomainName;
+                                        }
                                     }
                                 }
+                                if (!domainExists) {
+                                    domainsJsonChanged = true;
+                                    var domainJson = {
+                                        "DomainID": serverInformation.DomainID,
+                                        "DomainName": ""
+                                    };
+                                    DomainsJson.push(domainJson);
+                                }
+                                var serverOwner;
+                                if (serverInformation.SponserAccountID != null) {
+                                    var rawServerOwnerInformationdata = fs.readFileSync("Entities/Account/" + serverInformation.SponserAccountID + ".json");
+                                    serverOwnerInformation = JSON.parse(rawServerOwnerInformationdata);
+                                    serverOwner = serverOwnerInformation.Username
+                                } else {
+                                    serverOwner = "Unregistered";
+                                }
+                                if (serverInformation.Restriction == "open") {
+                                    var domain = {
+                                        "Domain Name": DomainName,
+                                        "Owner": serverOwner,
+                                        "Visit": "hifi://" + serverInformation.DomainID,
+                                        "People": serverInformation.TotalUsers
+                                    };
+                                    pageData.push(domain);
+                                }
                             }
-                            if (!domainExists) {
-                                domainsJsonChanged = true;
-                                var domainJson = {
-                                    "DomainID": serverInformation.DomainID,
-                                    "DomainName": ""
-                                };
-                                DomainsJson.push(domainJson);
-                            }
-                            var serverOwner;
-                            if (serverInformation.SponserAccountID != null) {
-                                var rawServerOwnerInformationdata = fs.readFileSync("Entities/Account/" + serverInformation.SponserAccountID + ".json");
-                                serverOwnerInformation = JSON.parse(rawServerOwnerInformationdata);
-                                serverOwner = serverOwnerInformation.Username
-                            } else {
-                                serverOwner = "Unregistered";
-                            }
-                            if (serverInformation.Restriction == "open") {
-                                var domain = {
-                                    "Domain Name": DomainName,
-                                    "Owner": serverOwner,
-                                    "Visit": "hifi://" + serverInformation.DomainID,
-                                    "People": serverInformation.TotalUsers
-                                };
-                                pageData.push(domain);
-                            }
-                        }
-                    });
-                    if (domainsJsonChanged) {
-                        fs.writeFile('Entities/Domains/Domains.json', JSON.stringify(DomainsJson, null, 2), function (err) {
-                            if (err) return console.log(err);
                         });
-                        domainsJsonChanged = false;
+                        if (domainsJsonChanged) {
+                            fs.writeFile('Entities/Domains/Domains.json', JSON.stringify(DomainsJson, null, 2), function (err) {
+                                if (err) return console.log(err);
+                            });
+                            domainsJsonChanged = false;
+                        }
+                        sendPage(JSON.stringify(pageData));
                     }
-                    sendPage(JSON.stringify(pageData));
-                }
-            });
-            return;
-        }
-        function sendPage(pageData) {
-            response.writeHead(200, { "Content-Type": "text/html" });
-            response.write(pageData);
-            response.end();
+                });
+                return;
+            }
+            function sendPage(pageData) {
+                response.writeHead(200, { "Content-Type": "text/html" });
+                response.write(pageData);
+                response.end();
+            }
+        } else if (request.method == "POST") {
+            let data = '';
+            request.on('data', chunk => {
+                data += chunk;
+            })
+            request.on('end', () => {
+                var pushedDomainId = JSON.parse(data).domainId;
+                var pushedDomainDomainName = JSON.parse(data).domainName;
+                fs.exists(path.join(process.cwd() + "/Entities/Domain/" + pushedDomainId + ".json"), function (exists) {
+                    if (exists) {
+                        var rawServerInformationdata = fs.readFileSync("Entities/Domain/" + pushedDomainId + ".json");
+                        serverInformation = JSON.parse(rawServerInformationdata);
+                        if (serverInformation.LastSenderKey.split(":")[0] == response.connection.remoteAddress.split(":")[3]) {
+                            var rawdata = fs.readFileSync('Entities/Domains/Domains.json');
+                            DomainsJson = JSON.parse(rawdata);
+                            for (let i = 0; i < DomainsJson.length; i++) {
+                                if (DomainsJson[i].DomainID == pushedDomainId) {
+                                    DomainsJson[i].DomainName = pushedDomainDomainName;
+                                }
+                            }
+                            fs.writeFile('Entities/Domains/Domains.json', JSON.stringify(DomainsJson, null, 2), function (err) {
+                                if (err) return console.log(err);
+                            });
+                        }
+                    }
+                });
+            })
         }
     }).listen(parseInt(port, 10));
 
